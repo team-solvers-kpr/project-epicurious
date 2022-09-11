e0870f426a7513a4e18f4547fd028390fcfa780a
shahriarAb <sm.abeed007@gmail.com>
2022-09-11 17:37:36 +0600

    first commit on abid branch

--- src/components/sharedPage/Navbar.js
+++ src/components/sharedPage/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div>
-            <h1>This is navbar section</h1>
+            <h1 className='text-zinc-600 text-4xl'>NavBar is designed by me...</h1>
         </div>
     );
 };
