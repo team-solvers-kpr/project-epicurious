14677e3ac744512c637da0d53c211b248fbf78b4
shafiuljony <shafiuljony12@gmail.com>
2022-09-12 18:38:16 +0600

    books section

--- src/components/SectionTitle.js
+++ src/components/SectionTitle.js
@@ -6,10 +6,11 @@ export default function Information({
     author = 'author name'
 }) {
   return (
-    <div className='section_title'>
-        <h2 className="uppercase">{heading}</h2>
-        <p>{title}</p>
-        <h3>{author}</h3>
+    <div className='section_title p-8'>
+        <h2 className="heading uppercase font-bold text-slate-300 ">{heading}</h2>
+        <p className=' title hover:underline text-2xl my-5'>{title}</p>
+        <p className='mb-5'>vartical content</p>
+        <h3 className=' authorname uppercase text-red-600'>{author}</h3>
     </div>
   )
 }
--- src/components/pages/Home/Books.js
+++ src/components/pages/Home/Books.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 import allBooksImg from '../../../assets/images/allBooks.png';
 import Information from '../../SectionTitle';
 
@@ -7,14 +6,14 @@ export default function Books() {
   return (
     <div>
         <div className="container">
-            <Link to="/" className="">
+            <a href='/'>
             <div className="books_img">
                 <img className="inline-block" src={allBooksImg} alt='All the books' />
             </div>
-            <div className="information">
-                <Information heading='shopping' title='34 New Baking Books Comming This Fall' author='By Jacqueline Raposo' />
+            <div className="information bg-white h-60 w-2/5 -mt-16 relative  ml-96">
+                <Information heading='shopping' title='34 New Baking Books Comming This Fall'  author='By Jacqueline Raposo' />
             </div>
-            </Link>
+            </a>
         </div>
     </div>
   )
