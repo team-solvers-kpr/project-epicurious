1d78a7eff94b3129974d8c77cf89d89a7f376209
shahriarAb <sm.abeed007@gmail.com>
2022-09-14 15:14:29 +0600

    banner initial design completed

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Banner from "./Banner";
 import HomeCard from "./HomeCard";
 import Product from "./Product";
 
 const Home = () => {
   return (
     <div>
+      <Banner></Banner>
       <HomeCard></HomeCard>
       <Product></Product>
 
