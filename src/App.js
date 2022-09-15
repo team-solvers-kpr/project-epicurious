6402d7b9a1c598fd3edb2d2a50f4e988f5e939d0
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-09-15 18:04:46 +0600

    signup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/pages/Home/Home";
 import Signin from "./components/pages/Register/Signin";
+import SignUp from "./components/pages/SignUp/SignUp";
 import Footer from "./components/sharedPage/Footer";
 import Navbar from "./components/sharedPage/Navbar";
 import NotFound from "./components/sharedPage/NotFound";
@@ -11,9 +12,9 @@ function App() {
     <div className="App">
       <Navbar></Navbar>
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/signin' element={<Signin></Signin>}></Route>
-        <Route path='*' element={<NotFound></NotFound>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
