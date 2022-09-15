593360ba08b3d394bb41fe6e8ec07c55a4a7ee5a
shafiuljony <shafiuljony12@gmail.com>
2022-09-15 12:03:13 +0600

    SignIn page start

--- src/components/pages/Home/NewestRecipes.js
+++ src/components/pages/Home/NewestRecipes.js
@@ -9,47 +9,49 @@ import 'swiper/css';
 
 export default function NewestRecipes() {
   return (
-    <div className='newest_recipes bg-slate-100'>
-        <div className='lg:m-40 sm:p-2 sm:m-28'>
-            <div className='recipes_header'>
-                <div className="divider md:inline-flex lg:inline-flex w-full mt-16 text-black">
-                    <h1 className='font-bold font-serif text-3xl'>Our Newest Recipes</h1>
+    <div className='p-14 newest_recipes bg-slate-100 md:mt-[36rem] sm:mt-[110rem]'>
+                <div className='recipes_header'>
+                    <div className="divider md:inline-flex lg:inline-flex w-full mt-16 sm:mt-12 text-black">
+                        <h1 className='font-bold font-serif text-3xl'>Our Newest Recipes</h1>
+                    </div>
+                </div>
+                <div className='recipes_allItems md:m-8 mb-8'>
+                    <Swiper
+                        spaceBetween={5}
+                        slidesPerView={5}
+                        breakpoints={{
+
+                            400: {
+                                slidesPerView: 2,
+                            },
+                            // when windows width is >= 640px
+                            640: {
+                            slidesPerView: 2,
+                            },
+                            // when windows width is >= 768px
+                            768: {
+                            slidesPerView: 3,
+                            },
+                            1200: {
+                            slidesPerView: 5,
+                            },
+                        }}
+                    >
+                    {recipesData.map((recipe, index) =>{
+                        if(index >= 5) return '';
+                        return (
+                            <SwiperSlide 
+                            key={recipe.id}>
+                                <Recipe 
+                                    title={recipe.title}
+                                    img={recipe.img}
+                                    heading={recipe.heading}
+                                />
+                            </SwiperSlide>
+                        )
+                    })}
+                    </Swiper>
                 </div>
             </div>
-            <div className='recipes_allItems md:m-8 mb-8'>
-                <Swiper
-                    spaceBetween={5}
-                    slidesPerView={5}
-                    breakpoints={{
-                        // when windows width is >= 640px
-                        640: {
-                        slidesPerView: 2,
-                        },
-                        // when windows width is >= 768px
-                        768: {
-                        slidesPerView: 3,
-                        },
-                        1200: {
-                        slidesPerView: 5,
-                        },
-                    }}
-                >
-                {recipesData.map((recipe, index) =>{
-                    if(index >= 5) return '';
-                    return (
-                        <SwiperSlide 
-                        key={recipe.id}>
-                            <Recipe 
-                                title={recipe.title}
-                                img={recipe.img}
-                                heading={recipe.heading}
-                            />
-                        </SwiperSlide>
-                    )
-                })}
-                </Swiper>
-            </div>
-        </div>
-    </div>
-  )
+    )
 }
--- src/components/pages/Register/Signin.js
+++ src/components/pages/Register/Signin.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Signin = () => {
-    return (
-        <div>
-            <h1>This is our signin page</h1>
+export default function Signin() {
+  return (
+    <div>
+        <div className='bg-zinc-500'>
+            <p className='uppercase p-8'>Email</p>
+            <form>
+            <input  className='border-4 border-solid h-12' type="text" placeholder='enter your email' />
+            </form>
+            {/* <a href="/" role="button" className="btn mt-5 bg-rose-600">Signin</a> */}
+            
+            <Link to='/'>
+            <button type='submit' className=' mb-5 bg-slate-900 h-12 text-center text-white uppercase mt-6 w-20 rounded hover:bg-rose-600'>Signin</button>
+            </Link>
+            
+            <h3 className='text-2xl'>Enter your email to go next step!</h3>
         </div>
-    );
-    
-};
-
-export default Signin;
+    </div>
+  )
+}
