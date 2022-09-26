fabf3e58e7d2c476d91e983ffe72e34f810b48d4
hparvez905 <hparvez905@gmail.com>
2022-09-26 15:54:18 +0600

    recipes and menu added done

--- src/components/pages/Home/Recipes_Menus.js
+++ src/components/pages/Home/Recipes_Menus.js
@@ -5,12 +5,12 @@ import { IoMdArrowDropright } from 'react-icons/io';
 const Recipes_Menus = () => {
   return (
     <div>
-        <div className=" text-4xl  font-semibold flex justify-start ">
+        <div className=" text-4xl  font-semibold flex justify-start ml-60 mt-10 ">
      <h1>RECIPES & MENUS</h1>
     </div>
-    <div className="flex justify-around p-20">
+    <div className="lg:flex justify-around lg:px-60 lg:py-10">
     <div className="p-10 ">
-        <h1 className='text-7xl  font-bold text-sky-500'>FIND A RECIPE</h1>
+        <h1 className='text-7xl  font-bold text-sky-700'>FIND A RECIPE</h1>
         <div className="flex item-center justify-center p-6 ">
         <div className="flex w-96 rounded border border-1  ">
             <input 
@@ -29,21 +29,42 @@ const Recipes_Menus = () => {
         </div>
         </div>
         
-       
+       <div className="hidden lg:block">
+        <div className="flex justify-evenly mb-4">
+        
+                       <h5 class='text-lg text-sky-600 font-semibold'> <a href='https://www.epicurious.com/search' class='hover:underline underline-offset-0   mx-2 p-1   leading-1' target='_blank' rel="noopener noreferrer">WHAT'S NEW</a></h5>
+                       <div className="border-r-2 border-gray-400 "></div>
+                       <h5 class=' text-lg text-sky-600 font-semibold'> <a href='https://www.epicurious.com/search?special-consideration=healthy' class='hover:underline underline-offset-0   mx-2 p-1   leading-1' target='_blank' rel="noopener noreferrer">HEALTHY</a></h5>
+                       <div className="border-r-2 border-gray-400 "></div>
+                       <h5 class='text-lg text-sky-600 font-semibold'> <a href='https://www.epicurious.com/search?special-consideration=quick-and-easy' class='hover:underline underline-offset-0    mx-2 p-1   leading-1' target='_blank' rel="noopener noreferrer">QUICK & EASY</a></h5>
+                       
+        </div>
+        <div className="flex justify-evenly">
+                       <h5 class='text-lg text-sky-600 font-semibold'> <a href='https://www.epicurious.com/holidays-events' class='hover:underline underline-offset-0    mx-2 p-1   leading-1' target='_blank' rel="noopener noreferrer">HOLIDAY</a></h5>
+                       <div className="border-r-2 border-gray-400 "></div>
+                       <h5 class='text-lg text-sky-600 font-semibold'> <a href='https://www.epicurious.com/search?special-consideration=wheat-gluten-free' class='hover:underline underline-offset-0    mx-2 p-1   leading-1' target='_blank' rel="noopener noreferrer">GLUTEN-FREE</a></h5>
+                       <div className="border-r-2 border-gray-400 "></div>
+                       <h5 class='text-lg text-sky-600 font-semibold'> <a href='https://www.epicurious.com/search?special-consideration=vegetarian' class='hover:underline underline-offset-0    mx-2 p-1   leading-1' target='_blank' rel="noopener noreferrer">VEGETARIAN</a></h5>
+                       
+        </div>
+       </div>
     </div>
-    <div className="border-r-2">
+
+
+    <div className="border-r-2 border-gray-300 ">
 
     </div>
     <div className="p-10">
-    <h1 className='text-7xl  font-bold text-orange-500'>CREATE A MENU</h1>
-       <div className="p-4">
-       <p className="text-xl">Use our new menu creator to create your own</p>
-        <p className="text-xl"> menu collection from any recipes and share it with friends!</p>
+    <h1 className='text-7xl  font-bold text-orange-600'>CREATE A MENU</h1>
+       <div className="p-4 mt-10">
+       <p className="text-xl font-semibold">Use our new menu creator to create your own</p>
+        <p className="text-xl font-semibold"> menu collection from any recipes and share it</p>
+        <p className="text-xl font-semibold">with friends!</p>
        </div>
 
        <div className="flex  justify-center">
-       <a href='https://www.condenast.com/careers' class='hover:underline underline-offset-0 text-xl text-orange-500 font-semibold mx-2 p-1' target='_blank' rel="noopener noreferrer">CREATE A MENU </a>
-        <IoMdArrowDropright className="text-2xl text-gray-400 mt-2"></IoMdArrowDropright>
+       <a href='https://id.condenast.com/interaction/nnPP-NADhxjdTFZ8GlWNk/email?xid=22d82be7-60f3-4b62-9cde-2bb784a73434&scope=openid%20offline_access&state=%7B%22redirectURL%22%3A%22%2Faccount%2Fsaved%22%7D&prompt=select_account%20consent&client_id=condenast.identity.bd96ef0e8186fcea33396720bc76b8b9&redirect_uri=https%3A%2F%2Fwww.epicurious.com%2Fauth%2Fcomplete&response_type=code' class='hover:underline underline-offset-0 text-xl text-orange-500 font-semibold mx-2 p-1' target='_blank' rel="noopener noreferrer">CREATE A MENU </a>
+        <IoMdArrowDropright className="text-2xl text-gray-400 mt-1 pt-1"></IoMdArrowDropright>
        </div>
     </div>
     </div>
