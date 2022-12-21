7140b436e4ddba7c14cc0632e99baa4f4d6e95eb
shafiuljony <shafiuljony12@gmail.com>
2022-12-21 16:55:19 +0600

    ingridenttop calling json data

--- src/components/pages/Ingredients/IngredientsTop.jsx
+++ src/components/pages/Ingredients/IngredientsTop.jsx
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Images from "../../../assets/Images/Brussels_sprouts_with_pistachios_and_lime_recipe_BA_111819.webp";
 import img from '../../../assets/Images/cornstarch-macro-051022.webp';
 
+
+
+
 const IngredientsTop = () => {
+
+
+  const [ ingredientsTopData, setIngredientsTopData ] = useState([]);
+useEffect(() => {
+
+  const fetchData = async () => {
+    const res = await fetch('./ingredients_cards.JSON');
+    const data = await res.json();
+    const sliceData = data.slice(0, 3);
+    setIngredientsTopData(sliceData);
+   
+  }
+  fetchData();
+
+}, []);
+
+console.log(ingredientsTopData)
   return (
     <div>
       
-    <div className='flex-col items-center flex justify-center mt-20 itop'>
+    <div className='flex-col items-center flex justify-center mt-20'>
         <h1 className='text-2xl uppercase font-bold'>ingredients</h1>
        <a href="/" >
-        <div className='relative flex justify-center outline-slate-50 outline-2 outline -outline-offset-[15px]'>
+        <div className='flex items-center justify-center flex-col outline-slate-50  outline-2 outline -outline-offset-[15px]'>
           <div className="ingridents-img">
             <img src={Images} alt="images" />
           </div>
-          <div className='bg-white ingridents-items absolute w-[85%]'>
+          <div className='bg-white -mt-24 w-[85%]'>
               <div className="p-6">
                   <h2 className="heading uppercase text-indigo-300">brussels sprout</h2>
-                  <p className=" title hover:underline text-2xl my-5 text-black font-semibold cursor-pointer pb-4">31 Ways to Make Crispy, Crunchy, Caramelized Brussels Sprouts
+                  <p className=" title hover:underline text-2xl my-5 text-black font-semibold cursor-pointer pb-2">31 Ways to Make Crispy, Crunchy, Caramelized Brussels Sprouts
                   </p>
-                  <hr className="mx-16 pb-4" />
+                  <hr className="mx-16" />
                   <div className='flex justify-center gap-2 pt-4'>
                     <h3 className="uppercase text-red-600">by the editors of epicurious </h3>
                     <span className='text-slate-300'>/ 11.15.22</span>
@@ -30,7 +50,7 @@ const IngredientsTop = () => {
      </div>
      <div>
 
-    <div className='flex justify-center flex-wrap card-top mt-20 py-16'>
+    <div className='flex justify-center flex-wrap mt-4'>
       <div className="p-4 sm:w-1/2 lg:w-1/3 ">
                 <a href='/' className="h-full border-2 cursor-pointer border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                   <img
