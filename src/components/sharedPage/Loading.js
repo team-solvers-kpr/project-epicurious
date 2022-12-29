c3826dcfccd1344bc5983d2720e2bd1565554bc4
team-solvers-kpr <113278332+team-solvers-kpr@users.noreply.github.com>
2022-12-29 18:33:07 +0600

    Update Loading.js

--- src/components/sharedPage/Loading.js
+++ src/components/sharedPage/Loading.js
@@ -1,13 +1,12 @@
 import React from "react";
-
-import "./Loading.css";
+import { BounceLoader } from "react-spinners";
 
 const Loading = () => {
-  return (
-    <div className="h-screen w-full flex justify-center items-center  ">
-      <div class="water"></div>
-    </div>
-  );
+   return (
+      <div className="flex justify-center my-32">
+         <BounceLoader color={"#36d7b7"} size={300} />
+      </div>
+   );
 };
 
 export default Loading;
